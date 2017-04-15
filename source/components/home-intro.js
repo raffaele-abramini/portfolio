c920e0b51e1d54b667fc9326fdd3e56ee7b2d9a8
oldirony <raffaele.abramini@gmail.com>
2017-04-15 15:22:52 +0100

    Home intro
    - add description

--- source/components/home-intro.js
+++ source/components/home-intro.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Intro = ({})=>{
     return (
-		<div className="c-logo">
-			<img src="/assets/images/logo@2x.png" alt=""/>
+		<div className="c-home-intro">
+			<h1 className="c-home-intro__logo">
+				<img src="/assets/images/logo@2x.png" alt="Old Irony"/>
+			</h1>
+
+			<h2 className="c-home-intro__description">
+				Web developer
+			</h2>
 		</div>
 	)
 };
