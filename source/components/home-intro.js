59d2637d1fad357de123661dd05fec46f3266154
oldirony <raffaele.abramini@gmail.com>
2017-04-17 20:09:18 +0100

    Style
    - add svg logo

--- source/components/home-intro.js
+++ source/components/home-intro.js
@@ -4,7 +4,9 @@ const Intro = ({})=>{
     return (
 		<div className="c-home-intro">
 			<h1 className="c-home-intro__logo">
-				<img src="/assets/images/logo@2x.png" alt="Old Irony"/>
+				<svg className="c-home-intro__logo-svg">
+					<use xlinkHref="#logo"></use>
+				</svg>
 			</h1>
 
 			<h2 className="c-home-intro__description">
