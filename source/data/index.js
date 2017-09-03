c7cccfa5e6424d9469977a48adf540b90545800a
oldirony <raffaele.abramini@gmail.com>
2017-09-03 15:52:09 +0100

    Portfolio
    - update links
    - add new Suzuki websites

--- source/data/index.js
+++ source/data/index.js
@@ -23,146 +23,158 @@ export default {
 	},
 	projects : [
 		{
+			"title": "Atv.suzuki.co.uk",
+			"url": "https://atv.suzuki.co.uk",
+			"tags": "Frontend development, gulp, jquery, oojs, sass",
+			"image": "/assets/images/portfolio/suzuki-bikes.jpg"
+		},
+		{
+			"title": "Marine.suzuki.co.uk",
+			"url": "https://marine.suzuki.co.uk",
+			"tags": "Frontend development, gulp, jquery, oojs, sass",
+			"image": "/assets/images/portfolio/suzuki-cars.jpg"
+		},
+		{
 			"title": "Bikes.suzuki.co.uk",
-			"url": "//bikes.suzuki.co.uk",
+			"url": "https://bikes.suzuki.co.uk",
 			"tags": "Frontend development, gulp, jquery, oojs, sass",
 			"image": "/assets/images/portfolio/suzuki-bikes.jpg"
 		},
 		{
 			"title": "Cars.suzuki.co.uk",
-			"url": "//cars.suzuki.co.uk",
+			"url": "https://cars.suzuki.co.uk",
 			"tags": "Frontend development, gulp, jquery, oojs, sass",
 			"image": "/assets/images/portfolio/suzuki-cars.jpg"
 		},
 		{
 			"title": "NFA",
-			"url": "//nfa.co.uk",
+			"url": "http://nfa.co.uk",
 			"tags": "Silverstripe based website (back & front end)",
 			"image": "/assets/images/portfolio/nfa.jpg"
 		},
 		{
 			"title": "Debenhams Finance",
-			"url": "//finance.debenhams.com",
+			"url": "http://finance.debenhams.com",
 			"tags": "Frontend modifications, server-side customised scripts for email tracking",
 			"image": "/assets/images/portfolio/debenhams.jpg"
 		},
 		{
 			"title": "The Flow Country",
-			"url": "//theflowcountry.org.uk",
+			"url": "http://theflowcountry.org.uk",
 			"tags": "Silverstripe based website (back & front end), custom Instagram import functions",
 			"image": "/assets/images/portfolio/flowcountry.jpg"
 		},
 		{
 			"title": "The Racegoers Club",
-			"url": "//racegoersclub.co.uk",
+			"url": "http://racegoersclub.co.uk",
 			"tags": "Custom e-commerce structure and Silverstripe website (back & front end), Angular app for Shopping basket",
 			"image": "/assets/images/portfolio/racegoers.jpg"
 		},
 		{
 			"title": "NBCU International",
-			"url": "//nbcuinternational.com/distribution-landing",
+			"url": "http://nbcuinternational.com/distribution-landing",
 			"tags": "SVG interactive map, Frontend modifications",
 			"image": "/assets/images/portfolio/nbc.jpg"
 		},
 		{
 			"title": "GBR",
-			"url": "//greatbritishracing.com/horse-racing/racecards",
+			"url": "http://greatbritishracing.com/horse-racing/racecards",
 			"tags": "Angular app for racecards and results.",
 			"image": "/assets/images/portfolio/gbr.jpg"
 		},
 		{
 			"title": "British Study Centres",
-			"url": "//british-study.com",
+			"url": "http://british-study.com",
 			"tags": "Frontend Development, Responsive, PHP, jQuery, JSON Multilingual System",
 			"image": "/assets/images/portfolio/bsc.jpg"
 		},
 		{
 			"title": "Blog - British Study Centres",
-			"url": "//british-study.com/blog",
+			"url": "http://british-study.com/blog",
 			"tags": "Wordpress, Development, Responsive, PHP, jQuery",
 			"image": "/assets/images/portfolio/bsc_blog.jpg"
 		},
 		{
 			"title": "Corriere di Como",
-			"url": "//corrieredicomo.it",
+			"url": "http://corrieredicomo.it",
 			"tags": "Wordpress, Development, Responsive, PHP, jQuery, Custom Backend",
 			"image": "/assets/images/portfolio/cdc.jpg"
 		},
 		{
 			"title": "Valpoci",
-			"url": "//valpoci.it",
+			"url": "http://valpoci.it",
 			"tags": "Wordpress, Development, Responsive, PHP, jQuery",
 			"image": "/assets/images/portfolio/valpoci.jpg"
 		},
 		{
 			"title": "Exa Pumps",
-			"url": "//exapumps.it",
+			"url": "http://exapumps.it",
 			"tags": "Wordpress, Design, Development, restricted areas, PHP, jQuery",
 			"image": "/assets/images/portfolio/exa.jpg"
 		},
 		{
 			"title": "Folatti",
-			"url": "//folatti.it",
+			"url": "http://folatti.it",
 			"tags": "Wordpress, Development, Responsive, user custom contents, PHP, jQuery",
 			"image": "/assets/images/portfolio/fola.jpg"
 		},
 		{
 			"title": "KPM Broaching",
-			"url": "//kpmbroaching.com",
+			"url": "http://kpmbroaching.com",
 			"tags": "Landing Page, Design, Development, Responsive, jQuery",
 			"image": "/assets/images/portfolio/kpm.jpg"
 		},
 		{
 			"title": "Dema Costruzioni",
-			"url": "//dema-costruzioni.com",
+			"url": "http://dema-costruzioni.com",
 			"tags": "Wordpress, Development, Responsive, PHP, jQuery",
 			"image": "/assets/images/portfolio/dema.jpg"
 		},
 		{
 			"title": "Tessile & Salute",
-			"url": "//tessileesalute.it",
+			"url": "http://tessileesalute.it",
 			"tags": "Wordpress, Development, Responsive, PHP, jQuery",
 			"image": "/assets/images/portfolio/tessile.jpg"
 		},
 		{
 			"title": "Immobiliare Morbegno",
-			"url": "//immobiliaremorbegno.it",
+			"url": "http://immobiliaremorbegno.it",
 			"tags": "Wordpress, Development, Responsive, PHP, jQuery",
 			"image": "/assets/images/portfolio/immob.jpg"
 		},
 		{
 			"title": "CP Technology",
-			"url": "//cptechnology.it",
+			"url": "http://cptechnology.it",
 			"tags": "Wordpress, Development, Responsive, PHP, jQuery",
 			"image": "/assets/images/portfolio/cptech.jpg"
 		},
 		{
 			"title": "Latteria Valtellina",
-			"url": "//latteriavaltellina.it",
+			"url": "http://latteriavaltellina.it",
 			"tags": "Wordpress, Development, Responsive, PHP, jQuery",
 			"image": "/assets/images/portfolio/latteria.jpg"
 		},
 		{
 			"title": "Latteria Valtellina: Progetto Scuola",
-			"url": "//scuola.latteriavaltellina.it",
+			"url": "http://scuola.latteriavaltellina.it",
 			"tags": "Wordpress, Development, Responsive, PHP, jQuery",
 			"image": "/assets/images/portfolio/scuolalat.jpg"
 		},
 		{
 			"title": "Miele Dell'Orto",
-			"url": "//mieledellorto.it",
+			"url": "http://mieledellorto.it",
 			"tags": "Wordpress, Development",
 			"image": "/assets/images/portfolio/miele.jpg"
 		},
 		{
 			"title": "BLP Costruzioni",
-			"url": "//blpcostruzioni.com",
+			"url": "http://blpcostruzioni.com",
 			"tags": "Wordpress, Development, PHP, jQuery",
 			"image": "/assets/images/portfolio/blp.jpg"
 		},
 		{
 			"title": "Okaypaper",
-			"url": "//www.okaypaper.com",
+			"url": "http://www.okaypaper.com",
 			"tags": "Wordpress, Development, PHP, jQuery",
 			"image": "/assets/images/portfolio/okaypap.jpg"
 		}
