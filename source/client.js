37b82051cbaa7b646a6573cca59c67c4272027a8
oldirony <raffaele.abramini@gmail.com>
2017-09-30 15:50:26 +0100

    Update dependencies
    - remove redundant history attribute from browserRouter
    - user reactDOM.hydrate instead of reactDOM.render

--- source/client.js
+++ source/client.js
@@ -1,23 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import createBrowserHistory from 'history/createBrowserHistory'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from'./store';
 import style from'./styles/style.scss';
 import Root from './root';
 
-
-const history = createBrowserHistory();
-
-
 const App = () => {
-	return <Provider store={store}>
-		<Router history={history}>
-			<Root/>
-		</Router>
-	</Provider>
+	return (
+		<Provider store={store}>
+			<Router>
+				<Root/>
+			</Router>
+		</Provider>
+	)
 };
 
 
-ReactDOM.render(<App/> , document.querySelector('.app'));
+ReactDOM.hydrate(<App/> , document.querySelector('.app'));
