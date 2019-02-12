bb1d86d2f573fe2608f601955fd7b67a38eed5fd
oldirony <raffaele.abramini@gmail.com>
2019-02-12 19:37:51 +0000

    Style tweaks and intro restructure

--- source/components/home-intro.js
+++ source/components/home-intro.js
@@ -1,19 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-const Intro = ({})=>{
-    return (
-		<div className="c-home-intro">
-			<h1 className="c-home-intro__logo">
-				<svg className="c-home-intro__logo-svg o-animate-on-load o-animate-on-load--1">
-					<use xlinkHref="#logo"></use>
-				</svg>
-			</h1>
+const Intro = () => (
+  <div className="c-home-intro">
+    <h1 className="c-home-intro__title o-animate-on-load o-animate-on-load--1">
+      Raffaele Abramini
+    </h1>
 
-			<h2 className="c-home-intro__description o-animate-on-load o-animate-on-load--2">
-				Web development
-			</h2>
-		</div>
-	)
-};
+    <h2 className="c-home-intro__description o-animate-on-load o-animate-on-load--2">
+      is a frontend engineer
+    </h2>
+  </div>
+)
 
-export default Intro;
+export default Intro
