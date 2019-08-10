a5c2722c07c8f51922a2f84e5f514875cb0e2ae5
oldirony <raffaele.abramini@gmail.com>
2019-08-10 14:42:40 +0100

    Fixing typo

--- source/components/home-about.js
+++ source/components/home-about.js
@@ -8,7 +8,7 @@ const About = ({}) => {
   className="u-highlight">software engineer</span> living in London. I’ve been working in the field of frontend development for more than 8 years. </p>
       <p className="o-animate-in-view o-animate-in-view--slide">I’ve worked for several design and UX companies, both in Italy and UK. I used my time not only on delivering high quality code, but also to align the design vision with actual development. The result of these efforts were consistent and accessible websites, applications and pattern libraries.</p>
       <p className="o-animate-in-view o-animate-in-view--slide">I used a multitude of libraries and tools: Backbone,js, Angular, Angular 2+, React, Flux, Redux, Grunt, Gulp, Webpack, Node.js.</p>
-      <p className="o-animate-in-view o-animate-in-view--slide">Since October 2018, I’m working at <span
+      <p className="o-animate-in-view o-animate-in-view--slide">Since October 2018, I’ve been working at <span
         className="u-highlight">Twilio</span> as a developer for Twilio Flex.</p>
     </div>
   )
