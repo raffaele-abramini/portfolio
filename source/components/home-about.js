6b1acade8ca79067f9db762f749e42b9dbf9d1a7
oldirony <raffaele.abramini@gmail.com>
2019-08-10 14:32:01 +0100

    Update bio

--- source/components/home-about.js
+++ source/components/home-about.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from 'react'
 
-const About = ({})=>{
-    return (
-		<div className="c-about-section">
-			<h3 className="c-about-section__title o-animate-in-view o-animate-in-view--slide">Hi! I am Raffaele!</h3>
-			<p className="o-animate-in-view o-animate-in-view--slide">I'm a 27 years old Italian <span className="u-highlight">frontend developer</span>. I've been professionally coding and building websites for  more than 5 years.</p>
-			<p className="o-animate-in-view o-animate-in-view--slide">I got a degree in communication design and patterns in September 2012. After almost 2 years working as fullstack Wordpress developer in my native valley, I moved to London looking for a more dynamic and challenging environment.</p>
-			<p className="o-animate-in-view o-animate-in-view--slide">I've been working as a Frontend developer since then, using several technology stacks: from jQuery to vanilla JS, from Angular 1 to React, from Grunt to Webpack, passing through Gulp.</p>
-			<p className="o-animate-in-view o-animate-in-view--slide">I'm currently working at <a href="http://foolproof.co.uk">Foolproof</a>, a UX agency. Here I've the chance of experimenting with new technologies and understanding in deep the world behind design and UX.</p>
-			<p className="o-animate-in-view o-animate-in-view--slide">You can find the source code of my portfolio on <a href="https://github.com/oldirony/portfolio" target="_blank">Github</a>.</p>
-		</div>
-	)
-};
+const About = ({}) => {
+  return (
+    <div className="c-about-section">
+      <h3 className="c-about-section__title o-animate-in-view o-animate-in-view--slide">Hi! I am Raffaele!</h3>
+      <p className="o-animate-in-view o-animate-in-view--slide">I’m an Italian <span
+  className="u-highlight">software engineer</span> living in London. I’ve been working in the field of frontend development for more than 8 years. </p>
+      <p className="o-animate-in-view o-animate-in-view--slide">I’ve worked for several design and UX companies, both in Italy and UK. I used my time not only on delivering high quality code, but also to align the design vision with actual development. The result of these efforts were consistent and accessible websites, applications and pattern libraries.</p>
+      <p className="o-animate-in-view o-animate-in-view--slide">I used a multitude of libraries and tools: Backbone,js, Angular, Angular 2+, React, Flux, Redux, Grunt, Gulp, Webpack, Node.js.</p>
+      <p className="o-animate-in-view o-animate-in-view--slide">Since October 2018, I’m working at <span
+        className="u-highlight">Twilio</span> as a developer for Twilio Flex.</p>
+    </div>
+  )
+}
 
-export default About;
+export default About
